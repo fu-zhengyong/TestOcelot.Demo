a6118e75d868f10a989998b8aa1d167207a19b9b
fuzy <fxy6781349@qq.com>
2019-09-06 15:37:43 +0800

    init

---
+++ TestOcelot.ClientService/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestOcelot.ClientService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ClinetService-value1", "ClinetService-value2" };
+        }
+    }
+}
---
+++ TestOcelot.ProductService/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestOcelot.ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ProductService-value1", "ProductService-value2" };
+        }
+    }
+}
